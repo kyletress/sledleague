f7dd1c5f77dec73fa87a66698a3d4fbba48cfe7c
Kyle Tress <kyletress@gmail.com>
2013-08-31 00:13:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sledleague::Application.config.secret_token = '81631f01a3b8047ebd4ff3f2b0a5b8b066edfc71ffb8590c6a0a16b5098385af1e8cfc70886780bafd84cd329d408735814b35f26bbad338ab02fd7eda2da308'
